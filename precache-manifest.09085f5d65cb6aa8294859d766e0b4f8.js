61ef91e9e28c606e9764afd7be057d08110a8b59
mona mohamed <monaelhoby@yahoo.com>
2020-02-01 17:17:34 +0200

    Updates

---
+++ precache-manifest.09085f5d65cb6aa8294859d766e0b4f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8b05a0e1fd2d3c2c9da5066bbaff738",
+    "url": "/e-commerce/index.html"
+  },
+  {
+    "revision": "9bb852e0084bab3d80db",
+    "url": "/e-commerce/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "edf872fcb02d590152ff",
+    "url": "/e-commerce/static/css/main.dc246f49.chunk.css"
+  },
+  {
+    "revision": "9bb852e0084bab3d80db",
+    "url": "/e-commerce/static/js/2.5593e211.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/e-commerce/static/js/2.5593e211.chunk.js.LICENSE"
+  },
+  {
+    "revision": "edf872fcb02d590152ff",
+    "url": "/e-commerce/static/js/main.98d0589f.chunk.js"
+  },
+  {
+    "revision": "533725173623eb9453f6",
+    "url": "/e-commerce/static/js/runtime-main.53bcea4e.js"
+  },
+  {
+    "revision": "096c21e6eff5cbec82eba07e9ac1a5b7",
+    "url": "/e-commerce/static/media/logo.096c21e6.png"
+  }
+]);
